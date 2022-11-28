0fb9609727e2b6eb2d1203bcf062a7aa834b9f83
Christian Ortel <82163209+christianortel@users.noreply.github.com>
2022-11-28 00:11:26 -0500

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 
+`
+Help me write a blockchain themed twitter post based on the title below. Write this in the style of Sassal0x from the Daily Gwei. Please make it seem like the writer is very knowledgeable on the topic and did their research. 
+
+Title:
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 350,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
